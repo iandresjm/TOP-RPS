697689fdfc647c5a4359f361f0cd6c5d800e1566
Ignacio <iandresjm@gmail.com>
2022-09-27 18:35:41 -0300

    top-rps commit test

--- java.js
+++ java.js
@@ -4,25 +4,75 @@ function getComputerChoice(){
     return rpsl [Math.floor(Math.random()*rpsl.length)];
 }
 
-function playRound (prompt) {
-    
-    let playerText = window.prompt("Type: Rock, Paper or Scissors");
-    const playerSelection =playerText[0].toUpperCase() + playerText.slice(1);
+let plaScore = document.querySelector(".plaScore");
+let maScore = document.querySelector(".maScore");
+const buttons = document.querySelectorAll(".btnN");
+const btnReset = document.querySelector(".btnR")
+const btnChoice = document.querySelectorAll(".btnN")
 
-    const computerSelection = getComputerChoice();
+btnReset.addEventListener("click", () =>{
+    maScore.innerText = 0
+    plaScore.innerText = 0
+    for ( btnN of btnChoice){
+            btnN.classList.remove("btnC")
+    }       
+})
 
-     if (computerSelection === "Paper" && playerSelection === "Rock"){
-        console.log("You Lose! Paper beats Rock")
-     
-     }
-     
-}
+buttons.forEach((button) => {   
+    button.addEventListener('click',() => {
+        const computerSelection = getComputerChoice()
+        if (computerSelection === "Paper" && button.innerText === "Rock"){
+            console.log("You Lose! Paper beats Rock")
+            maScore.innerText +++ 1
+            if (maScore.innerText == "5"){
+                maScore.innerText = "Machine Wins!"
+                for (const btnN of btnChoice){
+                btnN.classList.add("btnC")}
+        
+        }}else if (computerSelection === "Scissors" && button.innerText === "Paper"){
+            console.log("You Lose! Scissors beats Paper")
+            maScore.innerText +++ 1
+            if (maScore.innerText == "5"){
+                maScore.innerText = "Machine Wins!"
+                for (const btnN of btnChoice){
+                btnN.classList.add("btnC")}
 
-function game (){
-    
-    for (let i = 0; i < 5; i++){
-        playRound()
+        }}else if (computerSelection === "Rock" && button.innerText === "Scissors"){
+            console.log("You Lose! Rock beats Scissors")
+            maScore.innerText +++ 1
+            if (maScore.innerText == "5"){
+                maScore.innerText = "Machine Wins!"
+                for (const btnN of btnChoice){
+                btnN.classList.add("btnC")}
 
-    }
+        }}else if (computerSelection === button.innerText){
+            console.log("Draw!")
 
-}
+        }else if (button.innerText === "Paper" && computerSelection === "Rock"){
+            console.log("You Win! Paper beats Rock")
+            plaScore.innerText +++ 1
+            if (plaScore.innerText == "5"){
+                plaScore.innerText = "You Win!"
+                for (const btnN of btnChoice){
+                btnN.classList.add("btnC")}
+
+        }}else if (button.innerText === "Scissors" && computerSelection === "Paper"){
+            console.log("You Win! Scissors beats Paper")
+            plaScore.innerText +++ 1
+            if (plaScore.innerText == "5"){
+                plaScore.innerText = "You Win!"
+                for (const btnN of btnChoice){
+                btnN.classList.add("btnC")}
+            
+        }}else if (button.innerText === "Rock" && computerSelection === "Scissors"){
+            console.log("You Win! Rock beats Scissors")
+            plaScore.innerText +++ 1
+            if (plaScore.innerText == "5"){
+                plaScore.innerText = "You Win!"
+                for (const btnN of btnChoice){
+                btnN.classList.add("btnC")}
+                
+            }
+        }
+    })    
+ })
